57dcd8c4160b06f4153d5f5991f436048f9ffb17
izacarias <iuli.zacarias@gmail.com>
2023-01-19 15:27:55 +0100

    Applying the changes requested by @fcarp10

--- src/main/java/optimizer/algorithms/VariablesAlg.java
+++ src/main/java/optimizer/algorithms/VariablesAlg.java
@@ -175,13 +175,8 @@ public class VariablesAlg {
          pathDelay += (double) link.getAttribute(LINK_DELAY) * 1000; // in ms
       serviceDelay += pathDelay;
       // migration delay
-      if (fXSVinitial != null) {
-         double downtime;
-         try {
-            downtime = (double) service.getAttribute(SERVICE_DOWNTIME);
-         } catch (NullPointerException e) {
-            downtime = 0;
-         }
+      if (fXSVinitial != null && service.getAttributes().containsKey(SERVICE_DOWNTIME)) {
+         double downtime = (double) service.getAttribute(SERVICE_DOWNTIME);
          double totalServiceDowntime = 0;
          for (int x = 0; x < pm.getServers().size(); x++)
             for (int v = 0; v < service.getFunctions().size(); v++)
